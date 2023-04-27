9141aef93250e82533914164b61c3a0d336f9f9e
kyleraykbs <44762123+kyleraykbs@users.noreply.github.com>
2023-04-27 14:38:38 -0700

    minor server configuration

--- src/launcher.py
+++ src/launcher.py
@@ -8,6 +8,7 @@ builtserverdir = ".builtserver/"
 
 if __name__ == "__main__":
     gamepath = os.path.expanduser("~/.steam/steam/steamapps/common/Portal 2/")
-    if os.path.exists(builtserverdir):
-        shutil.rmtree(builtserverdir)
     buildserver(gamepath, builtserverdir)
+    #* launch code below
+    args = "+mp_dev_wait_for_other_player 0 +map mp_coop_lobby_3 -textmode"
+    os.system("wine " + builtserverdir + "portal2.exe " + args)
--- src/modules/builder.py
+++ src/modules/builder.py
@@ -3,7 +3,7 @@
 #* PURPOSE: take a pre-existing portal 2 and strip it down into a dedicated
 #* server-like environment that run's headlessly and without a steam account
 
-import os
+import os, time, shutil
 from modules.functions import *
 
 def buildserver(gamepath, outputpath):
@@ -12,24 +12,31 @@ def buildserver(gamepath, outputpath):
     f.close()
 
     #* create whatever paths need to exist
-    if os.path.exists(gamepath):
-        print(gamepath)
-    else:
+    if not os.path.exists(gamepath):
         print("path doesn't exist")
         exit()
 
     if os.path.exists(outputpath):
-        print(outputpath)
-    else:
-        os.mkdir(outputpath)
+        shutil.rmtree(outputpath)
+    os.mkdir(outputpath)
 
     #* assemble the base server
+    print("symlinking server")
+    oldtime = time.time()
     for fl in filelist:
         symlink(gamepath + fl, outputpath + fl)
-
+    print("symlinking finished in: " + str(time.time() - oldtime) + " seconds!")
+    
     #* for whatever reason steam_api.dll doesn't work with a symlinked steam.inf file so we need to copy it instead
     shutil.copy(gamepath + "portal2/steam.inf", outputpath + "portal2/steam.inf")
 
     #* download goldberg into the correct folder
-    print("Downloading Goldberg...")
-    downloadgoldberg(outputpath + "bin/steam_api.dll")
+    if not os.path.isfile("goldberg.dll"):
+        print("downloading goldberg...")
+        downloadgoldberg("goldberg.dll")
+        print("downloaded goldberg")
+    shutil.copy("goldberg.dll", outputpath + "bin/steam_api.dll")
+    os.makedirs(outputpath + "bin/steam_settings")
+    f = open(outputpath + "bin/steam_settings/force_account_name.txt", "w")
+    f.write("server")
+    f.close()
