79428d5bdded7e755c093d3c46bad0c20cee5c90
kyleraykbs <44762123+kyleraykbs@users.noreply.github.com>
2023-05-04 04:16:22 -0700

    fixed infinite window hide loop and added console line hooking

--- src/launcher.py
+++ src/launcher.py
@@ -4,35 +4,62 @@ import subprocess
 import multiprocessing
 import signal
 import sys
+import time
 import win32gui #pip install pywin32
 import psutil #pip isntall psutil
 from modules.builder import buildserver
+from modules.functions import getsystem
 
-system = platform.system().lower()
-linux = "linux"
-windows = "windows"
-
-if system == "darwin": 
+if getsystem() == "darwin": 
     print("We currently do not support MacOS hosting, nor do we plan to. However, you can still join other P2MM servers from a mac client.")
     exit()
-elif system == windows:
+elif getsystem() == "windows":
     gamepath = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Portal 2\\"
-elif system == linux:
+elif getsystem() == "linux":
     gamepath = os.path.expanduser("~/.steam/steam/steamapps/common/Portal 2/")
 
+gamehidden = 0 # i have this varible outside of its respective function as the enumwindows fuction appears not to be able to handle global imports from local functions
 os.chdir(os.path.abspath(os.path.dirname(__file__)))
 
-builtserverdir = ".builtserver" + os.sep
+bsdir = ".builtserver" + os.sep
 
-if __name__ == "__main__":
-    buildserver(gamepath, builtserverdir)
-    #* launch code below
-    args = "+echo p2mm +mp_dev_wait_for_other_player 0 +map mp_coop_lobby_3 +developer 1 -textmode -novid -nosound -nojoy -noipx -nopreload -norebuildaudio -condebug -refresh 30 -allowspectators -threads " + str(multiprocessing.cpu_count())
+def createlockfile(pid):
+    f = open("p2mm.lock", "w")
+    game = psutil.Process(pid)
+    for child in game.children():
+        f.write(str(child.pid) + "\n")
+    f.close()
 
-    if system == linux:
+def destroylockfile():
+    os.remove("p2mm.lock")
+
+def handlelockfile():
+    if os.path.exists("p2mm.lock"):
+        print("found lock file possible zombie instance")
+        f = open("p2mm.lock", "r")
+        pids = f.read().strip().split("\n")
+        f.close()
+        for pid in pids:
+                try:
+                    psutil.Process(int(pid)).kill()
+                    psutil.Process(int(pid)).wait()
+                    print("cleaned up zombie instance")
+                except:
+                    print("zombie instance already died")
+        destroylockfile()
+            
+
+
+def launchgame(builtserverdir, args):
+    handlelockfile() # clean up any possible zombie instances
+    buildserver(gamepath, bsdir) # build the serverfiles
+
+    # launch the game
+    if getsystem() == "linux":
         os.system("wine " + builtserverdir + "portal2.exe " + args)
-    elif system == windows:
+    elif getsystem() == "windows":
         process = subprocess.Popen('"' + builtserverdir + 'portal2.exe" ' + args, shell=True)
+        
         print("game running press CTRL+C to terminate")
 
         def kill_game():
@@ -44,21 +71,47 @@ if __name__ == "__main__":
             print("\n>EXIT SIGNAL RECIVED TERMINATING<\n")
             print("closing game")
             kill_game()
+            destroylockfile()
             print("closed")
             print("exiting")
             sys.exit(0)
         signal.signal(signal.SIGINT, sigint_handler)
 
         # hide the window after it opens
-        hidden = False
         def winEnumHandler( hwnd, ctx ):
+            global gamehidden
             if win32gui.IsWindowVisible( hwnd ):
-                if ".builtserver" in win32gui.GetWindowText( hwnd ):
-                    win32gui.ShowWindow(hwnd, False) # hide the window
-                    hidden = True
+                    if ".builtserver" in win32gui.GetWindowText( hwnd ):
+                        win32gui.ShowWindow(hwnd, False) # hide the window
+                        gamehidden += 1
 
         # if the window is not hidden itterate through all visable windows and try to hide it
-        while not hidden:
-            win32gui.EnumWindows( winEnumHandler, None )
+        print("waiting for windows to start so we can hide them")
+        while (gamehidden < 3):
+            output = win32gui.EnumWindows( winEnumHandler, None )
+        print("all windows hidden")
+        createlockfile(process.pid) # once the game is fully up create a lockfile to deal with zombie instances
 
-        
+curconsoleline = 0
+def getnewconsolelines(confile):
+    global curconsoleline
+    if os.path.isfile(confile):
+        f = open(confile, "r")
+        consolelines = f.read().strip().split("\n")
+        f.close()
+        if len(consolelines) > curconsoleline:
+            diff = len(consolelines) - curconsoleline
+            newlines = consolelines[-diff:]
+            curconsoleline = len(consolelines)
+            return newlines
+    return [] #return nothing if blank
+
+if __name__ == "__main__":
+    #* launch code below
+    args = "+echo p2mm +mp_dev_wait_for_other_player 0 +map mp_coop_lobby_3 +developer 1 -textmode -novid -nosound -nojoy -noipx -nopreload -norebuildaudio -condebug -refresh 30 -allowspectators -threads " + str(multiprocessing.cpu_count())
+    launchgame(bsdir, args)
+    print("launched game hooking console lines")
+    while True:
+        for line in getnewconsolelines(bsdir + "portal2" + os.sep + "console.log"):
+            print(line)
+        time.sleep(0.25)
