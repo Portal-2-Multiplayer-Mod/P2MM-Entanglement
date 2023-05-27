7a71c239a81ff4b10810947e4675cc9bcdae46e8
kyleraykbs <44762123+kyleraykbs@users.noreply.github.com>
2023-05-26 18:51:12 -0700

    added mounter and minor patching

---
+++ src/development/remount.py
@@ -0,0 +1,30 @@
+# DEVELOPER FILE
+# purpose: remount the files to the game after a change
+
+import os,shutil
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+os.chdir("..")
+
+game = ".builtserver"
+files = "modfiles"
+
+def get_all_files(directory):
+    file_list = []
+    for root, directories, files in os.walk(directory):
+        for file in files:
+            file_path = os.path.join(root, file)
+            file_list.append(file_path)
+    
+    return file_list
+
+for file in get_all_files(files):
+    print(file)
+    if not (os.path.isfile(files + os.sep + file)):
+        continue
+
+    if os.path.exists(game + os.sep + file):
+        os.remove(game + os.sep + file)
+
+    shutil.copyfile(files + os.sep + file, game + os.sep + file)
+    print(file)
--- src/modfiles/portal2/scripts/vscripts/mapspawn.nut
+++ src/modfiles/portal2/scripts/vscripts/mapspawn.nut
@@ -1,1 +1,13 @@
 printl("==== calling p2mm")
+
+lastmessage <- ""
+
+function ChatCommands(var1, var2) {
+    SendToChat("entindex " + var1.tostring(), 0)
+    SendToChat("message " + var2.tostring(), 0)
+    lastmessage = var2.tostring()
+}
+
+function returnsomedata() {
+    printl(lastmessage)
+}
