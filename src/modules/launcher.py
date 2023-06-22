1e7c5b1bf59c9a927cb9cc63b6357e19f5e03c89
cabiste <ahmedjj354@gmail.com>
2023-06-22 20:24:16 +0100

    partially cleaned 'launcher.py'

--- src/modules/launcher.py
+++ src/modules/launcher.py
@@ -1,167 +1,214 @@
-from collections.abc import Callable, Iterable, Mapping
-import os
-import platform
-import subprocess
-import multiprocessing
-import signal
-import sys
-import time
-import random
-import string
-import threading
-from typing import Any
+import os, subprocess, multiprocessing, signal, sys, time, random, threading, psutil
 
-import psutil #!pip isntall psutil
-from modules.builder import buildServer
 from modules.functions import GetSystem
-import modules.functions as functions
+from modules.builder import BuildServer
 from modules.logging import log
+import modules.functions as fn
 
+#? why?
 random.seed(time.time())
 
-gameisrunning = False
-RconReady = False
+IsGameRunning: bool = False
+IsRconReady: bool = False
+BuiltServerPath: str = ".builtserver" + os.sep
+RconPassword: str = ""
+RconPort: int = 3280
+P2ConsoleLogPath: str = BuiltServerPath + "portal2" + os.sep + "console.log"
+CurConsoleLine: int = 0
+System: str = GetSystem()
 
-if GetSystem() == "darwin":
-    log("We currently do not support MacOS hosting, nor do we plan to. However, you can still join other P2MM servers from a mac client.")
-    exit()
-elif GetSystem() == "windows":
-    import win32gui #!pip install pywin32
-    gamepath = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Portal 2\\"
-elif GetSystem() == "linux":
-    gamepath = os.path.expanduser("~/.local/share/Steam/steamapps/common/Portal 2/")
+if System == "darwin":
+    log(
+        "We currently do not support MacOS, nor do we plan to. However, you can still join other P2MM servers from a mac client."
+    )
+    exit(0)
 
-gamehidden = 0 # i have this varible outside of its respective function as the enumwindows fuction appears not to be able to handle global imports from local functions
+# todo: make the game path configurable
+if System == "windows":
+    # * pip install pywin32
+    import win32gui
 
-bsdir = ".builtserver" + os.sep
+    gamePath = "D:\\Program Files (x86)\\Steam\\steamapps\\common\\Portal 2\\"
 
-def createlockfile(pid):
-    global gameisrunning
-    f = open("p2mm.lock", "w", encoding="utf-8")
+elif System == "linux":
+    gamePath = os.path.expanduser("~/.local/share/Steam/steamapps/common/Portal 2/")
+
+
+# i have this variable outside of its respective function as the enumWindows function appears not to be able to handle global imports from local functions
+#! THIS VARIABLE ISN'T EVEN USED ANYWHERE
+gamehidden: int = 0
+
+
+def CreateLockFile(pid: int) -> None:
+    global IsGameRunning
+
+    fn.WriteToFile("p2mm.lock", f"{str(pid)}\n")
+
+    children = ""
     game = psutil.Process(pid)
-    f.write(str(pid) + "\n")
-    for child in game.children():
-        f.write(str(child.pid) + "\n")
-    f.close()
-    gameisrunning = True
 
-def destroylockfile():
+    for child in game.children():
+        children += str(child.pid) + "\n"
+
+    fn.WriteToFile("p2mm.lock", children)
+
+    IsGameRunning = True
+
+
+def DeleteLockFile() -> None:
     if os.path.exists("p2mm.lock"):
         try:
             os.remove("p2mm.lock")
         except Exception as e:
             log(e)
 
-def handlelockfile(intentionalkill = False):
-    global gameisrunning
-    global RconReady
+
+def HandleLockFile(intentionalKill=False) -> None:
+    global IsGameRunning, IsRconReady
 
     if os.path.exists("p2mm.lock"):
-        if not intentionalkill:
+        # ? what's the benefit of this check?
+        if not intentionalKill:
             log("found lock file possible zombie instance")
-        f = open("p2mm.lock", "r", encoding="utf-8")
-        pids = f.read().strip().split("\n")
-        f.close()
-        for pid in pids:
-                try:
-                    psutil.Process(int(pid)).kill()
-                    psutil.Process(int(pid)).wait()
-                    log("cleaned up zombie instance")
-                except:
-                    log("zombie instance already died")
-        destroylockfile()
-    gameisrunning = False
-    RconReady = False
 
-def randomword(length):
-    letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for i in range(length))
+        pids = fn.ReadFile("p2mm.lock").strip().split("\n")
+
+        # todo: we can probably use 'psutil.wait_procs()' instead
+        for pid in pids:
+            try:
+                psutil.Process(int(pid)).kill()
+                psutil.Process(int(pid)).wait()
+
+                log("cleaned up zombie instance")
+            except:
+                log("zombie instance already dead")
+
+        DeleteLockFile()
+
+    IsGameRunning = False
+    IsRconReady = False
+
 
 class RconTestThread(threading.Thread):
     def run(self):
-        global gameisrunning
-        global RconReady
-        global rconpasswd, rconport
-        shouldrun = True
-        while shouldrun and gameisrunning:
+        global IsGameRunning, IsRconReady, RconPassword, RconPort
+
+        shouldRun = True
+
+        while shouldRun and IsGameRunning:
             try:
-                output = functions.SendRcon('script printl("ready")', rconpasswd, rconport)
+                # ? why are we passing the port if it's the same as the default?
+                output = fn.SendRcon('script printl("ready")', RconPassword, RconPort)
+
                 if output.strip() == "ready":
                     log("rcon ready")
-                    RconReady = True
-                    shouldrun = False
-            except:
-                RconReady = False
+                    IsRconReady = True
+                    shouldRun = False
+
+            except Exception as e:
+                log(str(e), "error")
+                IsRconReady = False
+
 
 class WindowHideThread(threading.Thread):
     def run(self):
-        global gameisrunning
-        def winEnumHandler( hwnd, ctx ):
-            if win32gui.IsWindowVisible( hwnd ):
-                    if ".builtserver" in win32gui.GetWindowText( hwnd ):
-                        win32gui.ShowWindow(hwnd, False) # hide the window
-        while not gameisrunning: time.sleep(0.1)
-        log("game running trying to hide windows")
-        while (gameisrunning):
-            output = win32gui.EnumWindows( winEnumHandler, None )
+        global IsGameRunning
+
+        def winEnumHandler(hwnd: int, ctx):
+            if win32gui.IsWindowVisible(hwnd):
+                if ".builtserver" in win32gui.GetWindowText(hwnd):
+                    win32gui.ShowWindow(hwnd, False)  # hide the window
+
+        while not IsGameRunning:
             time.sleep(0.1)
 
-rconpasswd = ""
-rconport = 3280
-def launchgame(builtserverdir = bsdir, rconpasswdlocal="blank", launchargs="+hostname P2MM +sv_client_min_interp_ratio 0 +sv_client_max_interp_ratio 1000 +mp_dev_wait_for_other_player 0 +developer 1 +map mp_coop_lobby_3 -usercon -textmode -novid -nosound -nojoy -noipx -norebuildaudio -condebug -refresh 30 -allowspectators -threads " + str(multiprocessing.cpu_count()) + " +port 3280"):
-    global rconpasswd
-    global RconReady
-    RconReady = False
-    if rconpasswdlocal != "blank":
-        rconpasswd = rconpasswdlocal
-    else:
-        rconpasswd = randomword(8)
+        log("game is running, trying to hide windows")
 
-    handlelockfile() # clean up any possible zombie instances
+        while IsGameRunning:
+            # ? why do we need the 'output'?
+            output = win32gui.EnumWindows(winEnumHandler, None)
+            time.sleep(0.1)
 
-    buildServer(gamepath, "modfiles/", bsdir) # build the serverfiles
-    log("buildserver routine finished!")
-    launchargs = "+rcon_password " + rconpasswd + " " + launchargs
+
+def LaunchGame(
+    builtServerDir: str = BuiltServerPath,
+    rconLocalPassword: str = None,
+    launchArgs: str = None,
+) -> subprocess.Popen[bytes]:
+    global RconPassword, IsRconReady
+
+    IsRconReady = False
+
+    if rconLocalPassword == None:
+        rconLocalPassword = fn.GenerateRandomString(8)
+    RconPassword = rconLocalPassword
+
+    if launchArgs == None:
+        launchArgs = f"+hostname P2MM +sv_client_min_interp_ratio 0 +sv_client_max_interp_ratio 1000 +mp_dev_wait_for_other_player 0 +developer 1 +map mp_coop_lobby_3 -usercon -textmode -novid -nosound -nojoy -noipx -norebuildaudio -condebug -refresh 30 -allowspectators -threads {str(multiprocessing.cpu_count())} +port 3280"
+    # ? does the rcon password need to be at the start?
+    launchArgs = f"+rcon_password {RconPassword} {launchArgs}"
+
+    # clean up any possible zombie instances
+    HandleLockFile()
+
+    if not BuildServer(gamePath, "modfiles/", builtServerDir):
+        return
 
     # launch the game
-    if GetSystem() == "linux":
+    if System == "linux":
         log("Running In linux mode!")
-        def create_wine_prefix(folder_path):
-            # Check if the folder already exists
-            if os.path.exists(folder_path):
-                print("The Wine prefix folder already exists.")
+
+        def CreateWinePrefix(folderPath: str) -> bool:
+            if os.path.exists(folderPath):
+                log("The Wine prefix folder already exists.")
+
             else:
                 try:
-                    # Create the folder
-                    os.makedirs(folder_path)
-                    print("Wine prefix folder created successfully.")
-                except OSError:
-                    print("Failed to create Wine prefix folder.")
+                    os.makedirs(folderPath)
+                    log("Wine prefix folder created successfully.")
 
-        create_wine_prefix("p2mmwinepfx")
+                except Exception as e:
+                    log("Failed to create Wine prefix folder.")
+                    log(str(e), "error")
+                    return False
+            return True
+
+        if not CreateWinePrefix("p2mmwinepfx"):
+            return
+
         os.environ["WINEPREFIX"] = os.path.abspath("p2mmwinepfx")
-        process = subprocess.Popen('wine "' + builtserverdir + 'portal2.exe" ' + launchargs, shell=True)
-        while not os.path.isfile(confilepath): time.sleep(0.1)
+        process = subprocess.Popen(
+            f'wine "{builtServerDir}portal2.exe" {launchArgs}', shell=True
+        )
+
+        while not os.path.isfile(P2ConsoleLogPath):
+            time.sleep(0.1)
+
         log(str(process.pid) + "=============================")
-        createlockfile(process.pid)
-    elif GetSystem() == "windows":
-        process = subprocess.Popen('"' + builtserverdir + 'portal2.exe" ' + launchargs, shell=True)
+        CreateLockFile(process.pid)
 
-        log("game running press CTRL+C to terminate")
+    elif System == "windows":
+        process = subprocess.Popen(
+            f'"{builtServerDir}portal2.exe" ' + launchArgs, shell=True
+        )
 
-        def kill_game(pid):
+        log("game is running press CTRL+C to terminate")
+
+        def KillGame(pid: int):
             game = psutil.Process(pid)
             for child in game.children():
+                # i love coding keywords
                 child.kill()
 
         def sigint_handler(signal, frame):
-            log("\n>EXIT SIGNAL RECIVED TERMINATING<\n")
+            log("\n>EXIT SIGNAL RECEIVED TERMINATING<\n")
             log("closing game")
-            kill_game(process.pid)
-            destroylockfile()
+            KillGame(process.pid)
+            DeleteLockFile()
             log("closed")
             log("exiting")
             sys.exit(0)
+
         try:
             signal.signal(signal.SIGINT, sigint_handler)
         except:
@@ -169,45 +216,66 @@ def launchgame(builtserverdir = bsdir, rconpasswdlocal="blank", launchargs="+hos
 
         # hide the window after it opens
 
-
-        # if the window is not hidden itterate through all visable windows and try to hide it
+        # if the window is not hidden iterate through all visible windows and try to hide it
         log("waiting for game to start so we can hide and lock it")
 
-        windowhidethread = WindowHideThread()
-        windowhidethread.daemon = True
-        windowhidethread.start()
+        hideWindowThread = WindowHideThread()
+        hideWindowThread.daemon = True
+        hideWindowThread.start()
 
-        while not os.path.isfile(confilepath): time.sleep(0.1)
+        while not os.path.isfile(P2ConsoleLogPath):
+            time.sleep(0.1)
 
-        createlockfile(process.pid) # once the game is fully up create a lockfile to deal with zombie instances
+        # once the game is fully up create a lockfile to deal with zombie instances
+        CreateLockFile(process.pid)
         log("all windows hidden and game locked")
 
-    rcontestthread = RconTestThread()
-    rcontestthread.daemon = True
-    rcontestthread.start()
+    rconTestThread = RconTestThread()
+    rconTestThread.daemon = True
+    rconTestThread.start()
     return process
 
-confilepath = bsdir + "portal2" + os.sep + "console.log"
-curconsoleline = 0
-def getnewconsolelines(confile):
-    global curconsoleline
-    if os.path.isfile(confile):
-        f = open(confile, "r", encoding="latin-1")
-        consolelines = f.read().strip().split("\n")
-        f.close()
-        if len(consolelines) > curconsoleline:
-            diff = len(consolelines) - curconsoleline
-            newlines = consolelines[-diff:]
-            curconsoleline = len(consolelines)
+
+def GetNewConsoleLines(consoleFile: str) -> list[str]:
+    """gets the latest console lines from portal 2
+
+    Parameters
+    ----------
+    consoleFile : str
+        the path of the console logs' file
+
+    Returns
+    -------
+    list[str]
+        the new lines (empty list if there're none)
+    """
+
+    global CurConsoleLine
+
+    if os.path.isfile(consoleFile):
+        consoleLines = fn.ReadFile(consoleFile, _encoding="latin-1").strip().split("\n")
+
+        if len(consoleLines) > CurConsoleLine:
+            diff = len(consoleLines) - CurConsoleLine
+            newlines = consoleLines[-diff:]
+            CurConsoleLine = len(consoleLines)
             return newlines
-    return [] #return nothing if blank
+
+    return []
+
 
 if __name__ == "__main__":
+    # todo: FOR THE LOVE OF GOD STOP USING CHDIR
     os.chdir(os.path.abspath(os.path.dirname(__file__)))
-    #* launch code below
-    launchgame(bsdir)
-    log("launched game hooking console lines")
+
+    # * launch code below
+    LaunchGame(BuiltServerPath)
+
+    log("game started, hooking console lines")
+
     while True:
-        for line in getnewconsolelines(bsdir + "portal2" + os.sep + "console.log"):
+        for line in GetNewConsoleLines(
+            BuiltServerPath + "portal2" + os.sep + "console.log"
+        ):
             log(line, "game")
         time.sleep(0.1)
