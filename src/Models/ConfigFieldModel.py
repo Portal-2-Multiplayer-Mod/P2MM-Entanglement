cf21199a7ef7b76609eccbd0d7a9ec5c7769d8b3
cabiste <ahmedjj354@gmail.com>
2023-06-28 19:01:35 +0100

    cleaned the ConfigFieldModel.py

--- src/Models/ConfigFieldModel.py
+++ src/Models/ConfigFieldModel.py
@@ -3,63 +3,62 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 class ConfigFieldModel(QtWidgets.QWidget):
 
-    # def __init__(self, steps=5, *args, **kwargs):
-    #     super(ConfigFieldModel, self).__init__(*args, **kwargs)
-    def __init__(self, name: str, hint: str, value: any, customType: type):
+    def __init__(self, name: str, hint: str, value: any, customType: type) -> None:
         super(ConfigFieldModel, self).__init__()
+
+        self.setObjectName("configFieldModel")
+
         layout = QtWidgets.QVBoxLayout()
-        # self.setObjectName("configFieldModel")
-        self.Name = QtWidgets.QLabel(self)
-        sizePolicy = QtWidgets.QSizePolicy(
-            QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
+
+        layout.addWidget(self.CreateNameLabel(name))
+        layout.addWidget(self.CreateHintLabel(hint))
+        layout.addWidget(self.CreateValueField(customType, value))
+
+        spacerItem = QtWidgets.QSpacerItem(10, 20, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
+        layout.addItem(spacerItem)
+
+        self.setLayout(layout)
+
+    def CreateNameLabel(self, name: str) -> QtWidgets.QLabel:
+        Label = QtWidgets.QLabel(self)
+
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(
-            self.Name.sizePolicy().hasHeightForWidth())
-        self.Name.setSizePolicy(sizePolicy)
+        sizePolicy.setHeightForWidth(Label.sizePolicy().hasHeightForWidth())
+
+        Label.setSizePolicy(sizePolicy)
+
         font = QtGui.QFont()
         font.setPointSize(20)
         font.setBold(True)
         font.setWeight(75)
         font.setKerning(True)
-        self.Name.setFont(font)
-        self.Name.setAlignment(QtCore.Qt.AlignLeading |
-                               QtCore.Qt.AlignLeft | QtCore.Qt.AlignVCenter)
-        self.Name.setObjectName("Name")
-        layout.addWidget(self.Name)
-        self.Hint = QtWidgets.QLabel(self)
-        sizePolicy = QtWidgets.QSizePolicy(
-            QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
+
+        Label.setFont(font)
+        Label.setAlignment(QtCore.Qt.AlignLeading | QtCore.Qt.AlignLeft | QtCore.Qt.AlignVCenter)
+        Label.setObjectName("Label")
+        Label.setText(name)
+        return Label
+
+    def CreateHintLabel(self, hint: str) -> QtWidgets.QLabel:
+        Hint = QtWidgets.QLabel(self)
+
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(
-            self.Hint.sizePolicy().hasHeightForWidth())
-        self.Hint.setSizePolicy(sizePolicy)
+        sizePolicy.setHeightForWidth(Hint.sizePolicy().hasHeightForWidth())
+
+        Hint.setSizePolicy(sizePolicy)
+
         font = QtGui.QFont()
         font.setPointSize(8)
         font.setItalic(True)
-        self.Hint.setFont(font)
-        self.Hint.setObjectName("Hint")
-        layout.addWidget(self.Hint)
 
-
-        layout.addWidget(self.CreateValueField(customType, value))
-
-        # self.Value = QtWidgets.QLineEdit(self)
-        # self.Value.setMinimumSize(QtCore.QSize(0, 30))
-        # self.Value.setObjectName("Value")
-        # layout.addWidget(self.Value)
-
-        spacerItem = QtWidgets.QSpacerItem(
-            10, 20, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
-        layout.addItem(spacerItem)
-
-        self.setLayout(layout)
-
-        self.Name.setText(name)
-        self.Hint.setText(hint)
-        # self.Value.setText(str(value))
-
+        Hint.setFont(font)
+        Hint.setObjectName("Hint")
+        Hint.setText(hint)
+        return Hint
 
     def CreateValueField(self, customType: type, value: any) -> QtWidgets.QWidget:
         Value: QtWidgets.QWidget
@@ -73,9 +72,8 @@ class ConfigFieldModel(QtWidgets.QWidget):
             Value.setChecked(value)
         else:
             Value = QtWidgets.QLineEdit(self)
-            Value.setText(value)
+            Value.setText(str(value))
 
         Value.setMinimumSize(QtCore.QSize(0, 30))
         Value.setObjectName("Value")
         return Value
-
